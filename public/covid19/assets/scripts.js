cf4c174952187e9917c489008fa22b8b6707c2e6
José Eyzaguirre <josemanuel.eyzaguirre@gmail.com>
2022-01-11 21:28:16 -0300

    función de gráfico adicionada

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,1 +1,84 @@
-console.log("eeee")
+async function getData () {
+    const data = await fetch('/api/total')
+    const data2 = await data.json()
+    //console.log(data2)
+    const paises = data2.data
+    return paises
+    //console.log(paises)
+    //const nuevo = paises.filter( pais => pais.deaths >= 10000);
+    //console.log(nuevo)
+}
+
+async function dibujarGrafico () {
+    const paises = await getData();
+    const paisesGrafico = paises.filter( pais => pais.deaths >= 100000);
+    console.log(paisesGrafico)
+
+    const confirmados = [] //dataPoints
+    const muertos = [] //dataPoints
+
+    for (pais of paisesGrafico) {
+        confirmados.push(
+            { label: pais.location, y: pais.confirmed }
+        ) 
+        muertos.push(
+            { label: pais.location, y: pais.deaths }
+        ) 
+    }
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title:{
+            text: "Casos Confirmados y Muertos de COVID"
+        },	
+        axisY: {
+            title: "Casos COVID confirmados",
+            titleFontColor: "#4F81BC",
+            lineColor: "#4F81BC",
+            labelFontColor: "#4F81BC",
+            tickColor: "#4F81BC"
+        },
+        axisY2: {
+            title: "Muertos COVID",
+            titleFontColor: "#C0504E",
+            lineColor: "#C0504E",
+            labelFontColor: "#C0504E",
+            tickColor: "#C0504E"
+        },	
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor:"pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+            type: "column",
+            name: "Casos COVID confirmados",
+            legendText: "Casos COVID confirmados",
+            showInLegend: true, 
+            dataPoints: confirmados,
+        },
+        {
+            type: "column",	
+            name: "Número de muertos por COVID",
+            legendText: "Número de muertos por COVID",
+            axisYType: "secondary",
+            showInLegend: true,
+            dataPoints: muertos,
+        }]
+    });
+    chart.render();
+    
+    function toggleDataSeries(e) {
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        }
+        else {
+            e.dataSeries.visible = true;
+        }
+        chart.render();
+    }
+    
+}
+
+dibujarGrafico()
